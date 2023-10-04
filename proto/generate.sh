1a17d770b3239069964224a99807804e673e17d2
Daniel Hess <dan9186@gmail.com>
2023-10-04 11:32:23 -0700

    fix generation again

--- proto/generate.sh
+++ proto/generate.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 buf generate
-rm -rf ../listify/v1/*.pb.go
+rm -rf ../listify
 mv ./gen/github.com/pentops/protoc-gen-listify/listify ../
+cp ./static/v1/* ../listify/v1/
 rm -rf ./gen
