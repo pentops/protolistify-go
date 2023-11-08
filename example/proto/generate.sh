7c3091f05ec522e8ffe641f3e52cfd53da985f1d
Daniel Hess <dan9186@gmail.com>
2023-11-08 11:37:38 -0800

    better generation

--- example/proto/generate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-buf generate
-rm -rf ../api
-mv ./gen/github.com/pentops/protoc-gen-listify/example/api ../
-rm -rf ./gen
--- proto/generate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-buf generate
-rm -rf ../listify
-mv ./gen/github.com/pentops/protoc-gen-listify/listify ../
-cp ./static/v1/* ../listify/v1/
-rm -rf ./gen
